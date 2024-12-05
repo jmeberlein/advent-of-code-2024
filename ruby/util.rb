bb62b2d33ba4a1123d06b185e8529f9fc0b65bca
jmeberlein <jmeberlein@gmail.com>
2024-12-05 17:01:46 -0600

    Remove unneeded explicit block parameter

--- ruby/util.rb
+++ ruby/util.rb
@@ -1,5 +1,5 @@
 class Array
-    def sorted?(&cmp)
+    def sorted?
         (0...self.count-1).all? do |i|
             ((i+1)...self.count).all? do |j|
                 yield(self[i], self[j]) >= 0
