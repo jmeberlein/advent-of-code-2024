8b81cf4e9d97139fa27ce692fb7a511657d2457d
jmeberlein <jmeberlein@gmail.com>
2024-12-04 20:36:06 -0600

    Add Rust code

---
+++ rust/code_1a.rs
@@ -0,0 +1,33 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+pub fn main() {
+    let mut loc_a = Vec::new();
+    let mut loc_b = Vec::new();
+
+    if let Ok(file) = read_lines(env::args().nth(1).unwrap()) {
+        for line in file.flatten() {
+            let toks: Vec<&str> = line.split_whitespace().collect();
+            loc_a.push(toks[0].parse::<i32>().unwrap());
+            loc_b.push(toks[1].parse::<i32>().unwrap());
+        }
+    }
+
+    loc_a.sort();
+    loc_b.sort();
+
+    let mut sum = 0;
+    for i in 0..loc_a.len() {
+        sum += (loc_a[i] - loc_b[i]).abs();
+    }
+
+    println!("{}", sum);
+}
